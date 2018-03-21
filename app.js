2ee4e969a581a9a82b424ef4d8e3c9b1dfafa2c6
Collin Hintzke <collinhintzke5@gmail.com>
2018-03-21 09:21:41 -0700

    Chart finalized, css missing

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var picNames = [];
 var picVotes = [];
 var voteCounter = 0;
 var picList = document.getElementById('piclist');
-
 // constructor
 function Picture(name, filepath) {
   this.name = name;
@@ -13,19 +12,15 @@ function Picture(name, filepath) {
   this.views = 0
   this.votes = 0
   Picture.allPics.push(this)
- // picNames.push(this.name)
 }
 function xfer(){
   Picture.allPics.push(Picture.threePics[0]);
   Picture.allPics.push(Picture.threePics[1]);
   Picture.allPics.push(Picture.threePics[2]);
   Picture.threePics.splice(0, 3);
-
 }
-
 function resultsRender() {
   var arrayOfColors = ['red', 'blue', 'green', 'red', 'blue', 'green', 'red', 'blue', 'green', 'red', 'blue', 'green', 'red', 'blue', 'green', 'red', 'blue', 'green', 'red', 'blue'];
-  
   for(var i in Picture.allPics) {
     picNames.push(Picture.allPics[i].name)
     picVotes.push(Picture.allPics[i].votes);
@@ -48,6 +43,7 @@ function resultsRender() {
       scales: {
         yAxes: [{
           ticks: {
+            //integer intervals
             beginAtZero: true
           }
         }]
@@ -55,8 +51,6 @@ function resultsRender() {
     }
   })
 }
-
-
 
 new Picture('bag' , 'img/bag.jpg');
 new Picture('banana' , 'img/banana.jpg');
@@ -78,7 +72,6 @@ new Picture('unicorn' , 'img/unicorn.jpg');
 new Picture('usb' , 'img/usb.gif');
 new Picture('water-can' , 'img/water-can.jpg');
 new Picture('wine-glass' , 'img/wine-glass.jpg');
-
 // access the element from the DOM
 var imgElement1 = document.getElementById('pic1');
 var imgElement2 = document.getElementById('pic2');
@@ -86,47 +79,28 @@ var imgElement3 = document.getElementById('pic3');
 imgElement1.addEventListener('click', randomPicture);
 imgElement2.addEventListener('click', randomPicture);
 imgElement3.addEventListener('click', randomPicture);
-
-
-
 //On Load
 function load() {
-
-
-
-  //  else...
-  //how many pics you want displayed?
- // for (i = 0 ; i = 2 ; i ++) {
- //   var randomIndex = Math.floor(Math.random() * Picture.allPics.length);
-//        imgElementElement
- // }
-  
-  
   var randomIndex1 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement1.src = Picture.allPics[randomIndex1].filepath;
   imgElement1.alt = Picture.allPics[randomIndex1].name;
   Picture.allPics[randomIndex1].views ++;
   Picture.threePics.push(Picture.allPics[randomIndex1]);
   Picture.allPics.splice(randomIndex1, 1);
-  
   var randomIndex2 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement2.src = Picture.allPics[randomIndex2].filepath;
   imgElement2.alt = Picture.allPics[randomIndex2].name;
   Picture.allPics[randomIndex2].views ++;
   Picture.threePics.push(Picture.allPics[randomIndex2]);
   Picture.allPics.splice(randomIndex2, 1);
-  
   var randomIndex3 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement3.src = Picture.allPics[randomIndex3].filepath;
   imgElement3.alt = Picture.allPics[randomIndex3].name;
   Picture.allPics[randomIndex3].views ++;
   Picture.threePics.push(Picture.allPics[randomIndex3]);
   Picture.allPics.splice(randomIndex3, 1);
-  
 };
-
 load();
-
 // random picture generator
 function randomPicture() {
   if (voteCounter > 24) {
@@ -136,62 +110,33 @@ function randomPicture() {
     imgElement2.removeEventListener('click', randomPicture);
     imgElement3.removeEventListener('click', randomPicture);
   }
-      // for (var i = 0 ; i < Picture.allPics.length ; i++)
-      // var listElement = document.createElement('li');
-      // listElement.textContent = (Picture.allPics[i].votes + ' votes for ' + Picture.allPics[i].name);
-      // picList.appendChild(listElement);
-    else {
+  else {
   voteCounter ++
-  //event.target.votes ++
-  //Picture.threePics[event.target].votes ++
-  //event.target.votes.value ++
   for(var i in Picture.threePics)
     if(event.target.alt === Picture.threePics[i].name){
       Picture.threePics[i].votes ++
-
-     // console.log();
     }
-
   var randomIndex1 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement1.src = Picture.allPics[randomIndex1].filepath;
   imgElement1.alt = Picture.allPics[randomIndex1].name;
   Picture.allPics[randomIndex1].views ++;
   Picture.threePics.push(Picture.allPics[randomIndex1]);
   Picture.allPics.splice(randomIndex1, 1);
-  
   var randomIndex2 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement2.src = Picture.allPics[randomIndex2].filepath;
   imgElement2.alt = Picture.allPics[randomIndex2].name;
   Picture.allPics[randomIndex2].views ++;
   Picture.threePics.push(Picture.allPics[randomIndex2]);
   Picture.allPics.splice(randomIndex2, 1);
-  
   var randomIndex3 = Math.floor(Math.random() * Picture.allPics.length);
   imgElement3.src = Picture.allPics[randomIndex3].filepath;
   imgElement3.alt = Picture.allPics[randomIndex3].name;
   Picture.allPics[randomIndex3].views ++
   Picture.threePics.push(Picture.allPics[randomIndex3]);
   Picture.allPics.splice(randomIndex3, 1);
-  
-
   Picture.allPics.push(Picture.threePics[0]);
   Picture.allPics.push(Picture.threePics[1]);
   Picture.allPics.push(Picture.threePics[2]);
   Picture.threePics.splice(0, 3);
-  //cycle
-  console.log(randomIndex1,randomIndex2, randomIndex3);
-  console.log(Picture.allPics);
-  console.log(Picture.threePics);
-  console.log(voteCounter);
-//  if (voteCounter > 4){
- //   var li = document.('storeList')
 };
-
-//randomPicture();
-
-// render
-}
-//
-
-//function cycle() {
-
+};
